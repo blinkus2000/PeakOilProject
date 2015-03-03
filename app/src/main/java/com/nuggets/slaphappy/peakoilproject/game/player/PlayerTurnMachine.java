fee5e9e72feecb6f6dc223d9bff5449a2c3961df
Jack <johnscuncio@gmail.com>
2015-03-03 08:11:54 -0500

    player turn machine was not added to version control for some reason

---
+++ app/src/main/java/com/nuggets/slaphappy/peakoilproject/game/player/PlayerTurnMachine.java
@@ -0,0 +1,113 @@
+package com.nuggets.slaphappy.peakoilproject.game.player;
+
+import com.nuggets.slaphappy.peakoilproject.game.PeakOilEngine;
+import com.nuggets.slaphappy.peakoilproject.util.stateMachine.State;
+import com.nuggets.slaphappy.peakoilproject.util.stateMachine.StateMachine;
+import com.nuggets.slaphappy.peakoilproject.util.stateMachine.StateMachineException;
+import com.nuggets.slaphappy.peakoilproject.util.stateMachine.Transition;
+
+import java.util.LinkedList;
+
+/**
+ * com.nuggets.slaphappy.peakoilproject.game.player
+ * PeakOilProject
+ * Created by Jack on 02/03/2015.
+ */
+public class PlayerTurnMachine extends StateMachine<TurnAction> {
+    final LinkedList<PlayerTurn> players = new LinkedList<>();
+    protected int passes = 0;
+    final PeakOilEngine parent;
+    public PlayerTurnMachine(LinkedList<Player> playerList, PeakOilEngine parent) {
+        this.parent = parent;
+        for(int i = 0 ; i < playerList.size() ; i ++){
+            final int nextIndex = (i + 1)%players.size();
+            final PlayerTurn state = new PlayerTurn(playerList.get(i));
+            state.registerTransition(TurnType.END,new EndTurnTransition(nextIndex));
+            state.registerTransition(TurnType.PASS,new PassTransition(nextIndex));
+            state.registerTransition(state.getDispatchedAction(),new DispatchTransition(state));
+            players.add(state);
+        }
+    }
+    public class DispatchTransition extends Transition<TurnAction>{
+        final PlayerTurn state;
+
+        public DispatchTransition(PlayerTurn state) {
+            this.state = state;
+        }
+
+        @Override
+        protected State<TurnAction> handle(TurnAction item) {
+            try {
+                passes = 0;
+                PlayerTurnMachine.this.parent.doAction(state.getNextAction());
+            } catch (StateMachineException e) {
+            }
+            return state;
+        }
+    }
+    public abstract class TurnCompleteTransition extends Transition<TurnAction>{
+        final int nextIndex;
+
+        public TurnCompleteTransition(int nextIndex) {
+            this.nextIndex = nextIndex;
+        }
+    }
+    public class EndTurnTransition extends TurnCompleteTransition{
+
+        public EndTurnTransition(int nextIndex) {
+            super(nextIndex);
+        }
+
+        @Override
+        protected State<TurnAction> handle(TurnAction item) {
+            passes = 0;
+            return players.get(nextIndex);
+        }
+    }
+    public class PassTransition extends TurnCompleteTransition{
+
+        public PassTransition(int nextIndex) {
+            super(nextIndex);
+        }
+
+        @Override
+        protected State<TurnAction> handle(TurnAction item) {
+            passes++;
+            if(passes>=players.size()){
+                passes = 0;
+                try {
+                    PlayerTurnMachine.this.parent.doAction(PeakOilEngine.PhaseAction.ADVANCE);
+                } catch (StateMachineException e) {
+                    e.printStackTrace();
+                }
+                return players.get(0);
+            }else{
+                return players.get(nextIndex);
+            }
+        }
+    }
+    @Override
+    public State<TurnAction> getInitialState() {
+        return players.getFirst();
+    }
+    public enum TurnType implements TurnAction{
+        END,PASS;
+        TurnType(){}
+
+        @Override
+        public void cleanup() {}
+    }
+    public void currentPlayerPass() throws StateMachineException {
+        getCurrentState().doAction(this,TurnType.PASS);
+    }
+    public void currentPlayerEndTurn() throws StateMachineException {
+        getCurrentState().doAction(this,TurnType.END);
+    }
+    public void currentPlayerDoAction(PeakOilEngine.PhaseActionItem dispatchedAction) throws StateMachineException {
+        getCurrentPlayer().setNextAction(dispatchedAction);
+        getCurrentState().doAction(this,getCurrentPlayer().getDispatchedAction());
+    }
+    private final PlayerTurn getCurrentPlayer(){
+        return (PlayerTurn) getCurrentState();
+    }
+}
